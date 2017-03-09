934125bf31f8b79fe09f3d465e7315837edfc6c0
Fabrizio Ferro <ferro@ge.infn.it>
2017-03-09 14:40:08 +0100

    starting adding CTPPSPixelRawTiDigi

---
+++ EventFilter/CTPPSRawToDigi/interface/CTPPSPixelRawToDigi.h
@@ -0,0 +1,85 @@
+#ifndef CTPPSPixelRawToDigi_H
+#define CTPPSPixelRawToDigi_H
+
+/** \class CTPPSPixelRawToDigi_H
+ *  Plug-in module that performs Raw data to digi conversion 
+ *  for pixel subdetector
+ */
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/ESWatcher.h"
+#include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+
+//#include "CondFormats/DataRecord/interface/SiPixelFedCablingMapRcd.h"
+//#include "CondFormats/DataRecord/interface/SiPixelQualityRcd.h"
+#include "CondFormats/DataRecord/interface/CTPPSPixelReadoutRcd.h"
+#include "CondFormats/CTPPSReadoutObjects/interface/CTPPSPixelDAQMapping.h"
+
+#include "DataFormats/FEDRawData/interface/FEDRawDataCollection.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
+#include "FWCore/Utilities/interface/CPUTimer.h"
+
+
+//-- not needed??
+/*
+class SiPixelFedCablingTree;
+class SiPixelFedCabling;
+class SiPixelQuality;
+class TH1D;
+class PixelUnpackingRegions;
+*/
+//--
+
+
+class CTPPSPixelRawToDigi : public edm::stream::EDProducer<> {
+public:
+
+  /// ctor
+  explicit CTPPSPixelRawToDigi( const edm::ParameterSet& );
+
+  /// dtor
+  virtual ~CTPPSPixelRawToDigi();
+
+
+//-- not needed??
+//  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+  /// get data, convert to digis attach againe to Event
+  virtual void produce( edm::Event&, const edm::EventSetup& ) override;
+
+private:
+
+  edm::ParameterSet config_;
+
+//-- not needed??
+//  std::unique_ptr<SiPixelFedCablingTree> cabling_;
+//  const SiPixelQuality* badPixelInfo_;
+//  PixelUnpackingRegions* regions_;
+//--
+
+  edm::EDGetTokenT<FEDRawDataCollection> tFEDRawDataCollection; 
+
+//  TH1D *hCPU, *hDigi;
+//  std::unique_ptr<edm::CPUTimer> theTimer;
+  bool includeErrors;
+  bool useQuality;
+//  bool debug;
+  std::vector<int> tkerrorlist;
+  std::vector<int> usererrorlist;
+  std::set<unsigned int> fedIds;
+
+/*
+  edm::ESWatcher<SiPixelFedCablingMapRcd> recordWatcher;
+  edm::ESWatcher<SiPixelQualityRcd> qualityWatcher;
+*/
+  edm::InputTag label;
+  int ndigis;
+  int nwords;
+//  bool usePilotBlade;
+//  bool usePhase1;
+  std::string mappingLabel;
+};
+#endif
---
+++ EventFilter/CTPPSRawToDigi/test/ctppsPixelTest.py
@@ -0,0 +1,75 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("myRawToDigi")
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+
+############### using only CTPPS geometry
+process.load("Configuration.Geometry.geometry_CTPPS_cfi")
+process.load("CondFormats.CTPPSReadoutObjects.CTPPSPixelDAQMappingESSourceXML_cfi")
+#
+
+#process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+#process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+#process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+##process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load("Configuration.StandardSequences.Services_cff")
+
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100))
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1))
+
+process.source = cms.Source("PoolSource",
+# fileNames =  cms.untracked.vstring('file:rawdata.root')
+fileNames =  cms.untracked.vstring(
+"file:./PixelAlive_1294_153_RAW_v3.root"
+#  "rfio:/castor/cern.ch/cms/store/data/Run2012D/MinimumBias/RAW/v1/000/205/217/2EF61B7D-F216-E211-98C3-001D09F28D54.root",
+#  "rfio:/castor/cern.ch/cms/store/data/Run2012D/MinimumBias/RAW/v1/000/208/686/A88F66A0-393F-E211-9287-002481E0D524.root",
+#    "file:/afs/cern.ch/work/d/dkotlins/public/MC/mu/pt100_71_pre7/raw/raw2.root"
+ )
+)
+
+# Cabling
+#  include "CalibTracker/Configuration/data/Tracker_FakeConditions.cff"
+#process.load("CalibTracker.Configuration.SiPixel_FakeConditions_cff")
+#process.load("CalibTracker.Configuration.SiPixelCabling.SiPixelCabling_SQLite_cff")
+#process.GlobalTag.connect = "frontier://FrontierProd/CMS_COND_21X_GLOBALTAG"
+#process.GlobalTag.globaltag = "CRAFT_V3P::All"
+#process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
+#process.siPixelCabling.connect = 'sqlite_file:cabling.db'
+#process.siPixelCabling.toGet = cms.VPSet(cms.PSet(
+#    record = cms.string('SiPixelFedCablingMapRcd'),
+#    tag = cms.string('SiPixelFedCablingMap_v14')
+#))
+
+
+# Choose the global tag here:
+#process.GlobalTag.globaltag = "GR_P_V40::All"
+# for data in V7
+# process.GlobalTag.globaltag = "GR_R_71_V1::All"
+# for MC 
+##process.GlobalTag.globaltag = "MC_71_V1::All"
+
+process.load("EventFilter.CTPPSRawToDigi.CTPPSPixelRawToDigi_cfi")
+# for simultaions 
+#process.ctppsPixelDigis.InputLabel = 'ctppsPixelRawData'
+# for data
+process.ctppsPixelDigis.InputLabel = 'source'
+#process.ctppsPixelDigis.InputLabel = 'rawDataCollector'
+process.ctppsPixelDigis.IncludeErrors = False
+#process.ctppsPixelDigis.Timing = False 
+#process.siPixelDigis.UseCablingTree = True 
+
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring('ctppsPixelDigis'),
+    destinations = cms.untracked.vstring('r2d'),
+    r2d = cms.untracked.PSet( threshold = cms.untracked.string('WARNING'))
+)
+
+process.out = cms.OutputModule("PoolOutputModule",
+    fileName =  cms.untracked.string('file:digis.root'),
+#    fileName =  cms.untracked.string('file:/afs/cern.ch/work/d/dkotlins/public/data/digis/digis_1k.root'),
+    outputCommands = cms.untracked.vstring("keep *")
+)
+
+process.p = cms.Path(process.ctppsPixelDigis)
+process.ep = cms.EndPath(process.out)
