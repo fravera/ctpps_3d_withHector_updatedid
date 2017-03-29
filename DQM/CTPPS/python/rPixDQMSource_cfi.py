9c604241fb801dc1729dc87da915344a5ea73602
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2017-03-29 12:07:16 +0200

    fixing in dqm pys

--- DQM/CTPPS/python/rPixDQMSource_cfi.py
+++ DQM/CTPPS/python/rPixDQMSource_cfi.py
@@ -4,5 +4,5 @@ rPixDQMSource = cms.EDAnalyzer("RPixDQMSource",
 #    tagRPixDigi = cms.InputTag("RPixDetDigitizer", ""),
     tagRPixDigi = cms.InputTag("ctppsPixelDigis", ""),
     tagRPixCluster = cms.InputTag("clusterProd", ""),  
-    verbosity = cms.untracked.uint32(0)
+    verbosity = cms.untracked.uint32(2)
 )
