a9a7820964f823b92c96e260eb653495fdbd9301
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2017-04-03 16:14:44 +0200

    removed afs link, using eos one

--- EventFilter/CTPPSRawToDigi/test/test_pixels_only_cfg.py
+++ EventFilter/CTPPSRawToDigi/test/test_pixels_only_cfg.py
@@ -5,8 +5,8 @@ process = cms.Process("CTPPSRawToDigiTestPixelsOnly")
 # minimum of logs
 process.MessageLogger = cms.Service("MessageLogger",
     statistics = cms.untracked.vstring(),
-    destinations = cms.untracked.vstring('cout'),
-    cout = cms.untracked.PSet( threshold = cms.untracked.string('DEBUG') )
+    destinations = cms.untracked.vstring('cerr'),
+    cerr = cms.untracked.PSet( threshold = cms.untracked.string('DEBUG') )
 )
 
 # raw data source
@@ -14,7 +14,8 @@ process.source = cms.Source("PoolSource",
   fileNames = cms.untracked.vstring(
     #'file:/afs/cern.ch/user/j/jkaspar/public/run273062_ls0001-2_stream.root'
    # '/store/express/Run2016H/ExpressPhysics/FEVT/Express-v2/000/283/877/00000/4EE44B0E-2499-E611-A155-02163E011938.root'
-'file:/afs/cern.ch/work/k/kas/public/PXtrees/PixelAlive_1294_151_RAW_v2.root'
+#'file:/afs/cern.ch/work/k/kas/public/PXtrees/PixelAlive_1294_151_RAW_v2.root'
+'root://eoscms//eos/cms/store/user/jjhollar/012017_PixelDAQTests/PixelAlive_1294_151_RAW_v2.root'
   ),
 labelRawDataLikeMC = cms.untracked.bool(False) # for testing H8 data
 )
