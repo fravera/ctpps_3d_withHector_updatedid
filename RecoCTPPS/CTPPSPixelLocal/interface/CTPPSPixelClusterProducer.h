116b785ee65ab61d8592add41ef061b0b8c48978
Fabrizio Ferro <ferro@ge.infn.it>
2017-04-28 12:30:13 +0200

    fix double use of seeds in clusters

--- RecoCTPPS/CTPPSPixelLocal/interface/CTPPSPixelClusterProducer.h
+++ RecoCTPPS/CTPPSPixelLocal/interface/CTPPSPixelClusterProducer.h
@@ -1,7 +1,6 @@
 /**********************************************************************
  *
- * Author: F.Ferro - INFN Genova
- * September 2016
+ * Author: F.Ferro fabrizio.ferro@ge.infn.it - INFN Genova - 2017
  *
  **********************************************************************/
 #ifndef RecoCTPPS_CTPPSPixelLocal_CTPPSPixelClusterProducer
@@ -10,8 +9,6 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "DataFormats/Common/interface/DetSetVector.h"
 #include "DataFormats/Common/interface/DetSet.h"
-//#include "DataFormats/Common/interface/DetSetVectorNew.h"
-//#include "FWCore/Framework/interface/EDProducer.h"
 
 #include "FWCore/Framework/interface/stream/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
@@ -26,26 +23,17 @@
 #include "DataFormats/CTPPSReco/interface/CTPPSPixelCluster.h"
 #include "DataFormats/DetId/interface/DetId.h"
 
-#include "RecoCTPPS/CTPPSPixelLocal/interface/RPixDetClusterizer.h" 
-#include "DataFormats/Common/interface/DetSetVector.h"
-
 #include "DataFormats/CTPPSDetId/interface/CTPPSPixelDetId.h"
 
-#include "FWCore/Framework/interface/ESWatcher.h"
-#include "Geometry/VeryForwardGeometryBuilder/interface/TotemRPGeometry.h"
-#include "Geometry/VeryForwardRPTopology/interface/RPTopology.h"
-#include "Geometry/Records/interface/VeryForwardRealGeometryRecord.h"
-
-//#include "CondFormats/DataRecord/interface/CTPPSPixelReadoutRcd.h"
 #include "CondFormats/DataRecord/interface/CTPPSPixelDAQMappingRcd.h"
 #include "CondFormats/DataRecord/interface/CTPPSPixelAnalysisMaskRcd.h"
 #include "CondFormats/CTPPSReadoutObjects/interface/CTPPSPixelDAQMapping.h"
 #include "CondFormats/CTPPSReadoutObjects/interface/CTPPSPixelAnalysisMask.h"
 
+#include "RecoCTPPS/CTPPSPixelLocal/interface/RPixDetClusterizer.h" 
+
 #include <vector>
 #include <set>
-
-
 
 
 class CTPPSPixelClusterProducer : public edm::stream::EDProducer<>
@@ -63,8 +51,6 @@ private:
  
  edm::InputTag src_;
  edm::EDGetTokenT<edm::DetSetVector<CTPPSPixelDigi>> tokenCTPPSPixelDigi_;
-  
-// edm::ESWatcher<VeryForwardRealGeometryRecord> geometryWatcher;
 
  RPixDetClusterizer clusterizer_;
   
